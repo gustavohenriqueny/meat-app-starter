b897e730f46dff53fa2d1cf4d35e9c804314c186
gustavohenriqueny <gustavoheriqueny@gmail.com>
2022-10-26 22:21:42 -0300

    Aula 65, Componente de check box.

--- src/app/components/util/radio/radio.component.ts
+++ src/app/components/util/radio/radio.component.ts
@@ -1,15 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
 import {RadioOptionModel} from '../../../models/radio-option.model';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
 	selector: 'mt-radio',
-	templateUrl: './radio.component.html'
+	templateUrl: './radio.component.html',
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => RadioComponent),
+			multi: true
+		}
+	]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
 	@Input() options: RadioOptionModel[];
 
 	value: any;
+
+	onChange: any;
 
 	constructor() {
 	}
@@ -19,6 +29,21 @@ export class RadioComponent implements OnInit {
 
 	setValue(value: any) {
 		this.value = value;
+		this.onChange(value);
+	}
+
+	registerOnChange(fn: any): void {
+		this.onChange = fn;
+	}
+
+	registerOnTouched(fn: any): void {
+	}
+
+	setDisabledState(isDisabled: boolean): void {
+	}
+
+	writeValue(object: any): void {
+		this.value = object;
 	}
 
 }
