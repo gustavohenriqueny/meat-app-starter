c280fd2e46076bc424377f744ea8432bf6d5fa9b
gustavo.gomes <gustavo.gomes@basis.com.br>
2023-03-24 22:59:02 -0300

    81. Realizando o pre-carregamento de todos os modulos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {LOCALE_ID, NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
-import {RouterModule} from '@angular/router';
+import {PreloadAllModules, RouterModule} from '@angular/router';
 
 import {ROUTES} from './app.routes';
 import {AppComponent} from './app.component';
@@ -36,7 +36,7 @@ import {SharedModule} from './modules/shared/shared.module';
 		BrowserModule,
 		HttpModule,
 		SharedModule.forRoot(),
-		RouterModule.forRoot(ROUTES),
+		RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules}),
 	],
 	providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
 	bootstrap: [AppComponent]
