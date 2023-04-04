b0158b06bc6c5c65ebf71b6099aafa5d12ad6a29
gustavohenriqueny <gustavohenriqueny@gmail.com>
2023-04-04 16:48:24 -0300

    127. Atualização das dependências da aplicação.

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,29 +1,33 @@
 import {HttpErrorResponse} from '@angular/common/http';
-import {ErrorHandler, Injectable, Injector} from '@angular/core';
+import {ErrorHandler, Injectable, Injector, NgZone} from '@angular/core';
 import {NotificationService} from './services/notification.service';
 import {LoginService} from './services/login.service';
 
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
 
-    constructor(private notificationService: NotificationService, private injector: Injector) {
+    constructor(private notificationService: NotificationService,
+                private injector: Injector,
+                private zone: NgZone) {
         super();
     }
 
     handleError(errorResponse: HttpErrorResponse | any) {
         if (errorResponse instanceof HttpErrorResponse) {
             const message = errorResponse.error.message;
-            switch (errorResponse.status) {
-                case 401:
-                    this.injector.get(LoginService).handleLogin();
-                    break;
-                case 403:
-                    this.notificationService.notify(message || 'Não autorizado.')
-                    break;
-                case 404:
-                    this.notificationService.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.')
-                    break;
-            }
+            this.zone.run(() => {
+                switch (errorResponse.status) {
+                    case 401:
+                        this.injector.get(LoginService).handleLogin();
+                        break;
+                    case 403:
+                        this.notificationService.notify(message || 'Não autorizado.')
+                        break;
+                    case 404:
+                        this.notificationService.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.')
+                        break;
+                }
+            })
         }
         super.handleError(errorResponse);
     }
